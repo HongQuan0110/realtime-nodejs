5e729b37467c26c5ba3e6a54c521fac4518a1f1e
lvhquan0110 <lvhquan0110@gmail.com>
2019-07-22 20:47:12 +0700

    [1.0.0] Connect & create new item MongoDB

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-// var express = require('express');
 import express from "express";
+import ConnectDB from "./config/connectDB";
+import ContactModel from "./models/contact.model";
+
 let app = express();
 
-let port = 3000;
-let host = "localhost";
+// Connect to mongoDB
+ConnectDB();
 
-app.get('', (req, res, next) => {
-    res.end('<h1>Hello world</h1>');
+
+app.get('/test-db', async (req, res, next) => {
+    try {
+        let item = {
+            userId: "123123123",
+            contactId: "456456456"
+        }
+        let contact = await ContactModel.createNew(item)
+        res.send(contact);
+    } catch (error) {
+        console.log(error);
+    }
 })
 
-app.listen(port, host, () => {
-    console.log('Server listening on port: ', port);
+app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
+    console.log('Server listening on port: ', process.env.APP_PORT);
 })
--- src/models/contact.model.js
+++ src/models/contact.model.js
@@ -6,10 +6,15 @@ let ContactSchema = new Schema({
     userId: {type: String},
     contactId: {type: String},
     status: {type: Boolean, default: false},
-    username: {type: String},
     createdAt: {type: Number, default: Date.now},
     updatedAt: {type: Number, default: null},
     deletedAt: {type: Number, default: null}
 })
+
+ContactSchema.statics = {
+    createNew(item) {
+        return this.create(item);
+    }
+}
 
 module.exports = mongoose.model("contact", ContactSchema);
